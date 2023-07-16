020fb4e524ae9bae8c5429852ef3383747145135
James Hines <jameshines10@gmail.com>
2023-07-16 20:15:33 +0000

    Creates a sample with 4 inputs

--- a_single_neuron.py
+++ a_single_neuron.py
@@ -2,8 +2,20 @@ inputs = [1, 2, 3]
 weights = [0.2, 0.8, -0.5]
 bias = 2
 
-output = (inputs[0] * weights[0] +
-          inputs[1] * weights[1] +
-          inputs[2] * weights[2] + bias)
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+
+print(output)
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights1 = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+output = (
+    inputs[0] * weights1[0]
+    + inputs[1] * weights1[1]
+    + inputs[2] * weights1[2]
+    + inputs[3] * weights1[3]
+    + bias
+)
 
 print(output)
